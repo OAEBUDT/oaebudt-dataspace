28a29c93bab06afaedad1f49364da1b2c48e4ba0
mohamedkhalilbeldi <mohamedkhalilbeldi@think-it.io>
2025-03-08 02:27:02 +0100

    ci(vpc): create network resources for eks

--- infrastructure/eks/variables.tf
+++ infrastructure/eks/variables.tf
@@ -23,13 +23,11 @@ variable "vpc_name" {
 variable "eks_environment" {
   description = "Deployment environment"
   type        = string
-  default     = "development"
 }
 
 variable "eks_availability_zones" {
   description = "List of availability zones in the region where the EKS nodes will be deployed"
   type        = list(string)
-  default     = ["us-east-1a", "us-east-1b"]
 }
 
 variable "eks_name" {
@@ -47,16 +45,9 @@ variable "eks_version" {
 variable "vpc_cidr_block" {
   description = "The CIDR block to use for the vpc"
   type        = string
-  default     = "10.0.0.0/16"
 }
 
 variable "subnet_cidr_blocks" {
   description = "List of CIDR blocks to use for subnets"
   type        = list(string)
-  default = [
-    "10.0.0.0/20",
-    "10.0.16.0/20",
-    "10.0.32.0/20",
-    "10.0.48.0/20"
-  ]
 }

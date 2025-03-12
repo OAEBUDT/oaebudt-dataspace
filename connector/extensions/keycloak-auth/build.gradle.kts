6f00ab4454a63d8d2841add390ec50fff755deeb
mohamedkhalilbeldi <mohamedkhalilbeldi@think-it.io>
2025-03-12 14:55:16 +0100

    ci: add jacoco plugin

--- connector/extensions/keycloak-auth/build.gradle.kts
+++ connector/extensions/keycloak-auth/build.gradle.kts
@@ -1,6 +1,20 @@
 plugins {
     `java-library`
     id("application")
+    id("jacoco")
+}
+
+tasks.test {
+    useJUnitPlatform() // Ensures JUnit 5 is used
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
 }
 
 repositories {

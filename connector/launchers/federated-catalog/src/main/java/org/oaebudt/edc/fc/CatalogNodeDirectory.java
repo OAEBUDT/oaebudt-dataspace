9f2a77fc4d08abfd68e630fbd0bb0fb9c98b51c0
Juboy <aderibolejuwon@gmail.com>
2025-03-14 14:33:47 +0100

    Setup Federated catalog. Includes crawler that crawl through participant connectors

--- connector/launchers/federated-catalog/src/main/java/org/oaebudt/edc/fc/CatalogNodeDirectory.java
+++ connector/launchers/federated-catalog/src/main/java/org/oaebudt/edc/fc/CatalogNodeDirectory.java
@@ -5,19 +5,25 @@ import org.eclipse.edc.crawler.spi.TargetNodeDirectory;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CatalogNodeDirectory implements TargetNodeDirectory {
+
+    private final List<String> participantDspEndpoints;
+
+    public CatalogNodeDirectory(List<String> participantDspEndpoints) {
+        this.participantDspEndpoints = participantDspEndpoints;
+    }
 
     @Override
     public List<TargetNode> getAll() {
         List<String> protocolList = new ArrayList<>();
         protocolList.add("dataspace-protocol-http");
 
-        TargetNode participantNode = new TargetNode("https://w3id.org/edc/v0.0.1/ns/",
-                "participant-a",
-                "http://localhost:8192/api/dsp", protocolList); //targetUrl hardcoded for the mean time. Once we setup a did registry, we get the target node based on their dsp endpoints
-
-        return List.of(participantNode);
+        return participantDspEndpoints.stream()
+                .map(dspEndpoint -> new TargetNode("https://w3id.org/edc/v0.0.1/ns/",
+                        "participant-a",
+                        dspEndpoint, protocolList)).collect(Collectors.toList());
     }
 
     @Override

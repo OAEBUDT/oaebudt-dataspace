743db420072dff2646d065db7ba405684247f8bc
mohamedkhalilbeldi <mohamedkhalilbeldi@think-it.io>
2025-03-18 17:44:41 +0100

    ci: fix jacoco

--- connector/launchers/runtime-embedded/build.gradle.kts
+++ connector/launchers/runtime-embedded/build.gradle.kts
@@ -1,12 +1,27 @@
 plugins {
     `java-library`
     id("application")
+    id("jacoco")
     alias(libs.plugins.shadow)
+}
+
+tasks.test {
+    useJUnitPlatform() // Ensures JUnit 5 is used
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
 }
 
 repositories {
     mavenCentral()
 }
+
 
 dependencies {
     runtimeOnly(project(":launchers:controlplane")) {

4cd0714778809effb580da42f7e4f4b00f7d8b7d
mohamedkhalilbeldi <mohamedkhalilbeldi@think-it.io>
2025-03-27 12:38:09 +0100

    feat(did): implement did for 2 connectors

--- connector/extensions/dcp-impl/src/main/java/org/oaebudt/edc/dcp/core/DcpPatchExtension.java
+++ connector/extensions/dcp-impl/src/main/java/org/oaebudt/edc/dcp/core/DcpPatchExtension.java
@@ -1,17 +1,3 @@
-/*
- *  Copyright (c) 2024 Metaform Systems, Inc.
- *
- *  This program and the accompanying materials are made available under the
- *  terms of the Apache License, Version 2.0 which is available at
- *  https://www.apache.org/licenses/LICENSE-2.0
- *
- *  SPDX-License-Identifier: Apache-2.0
- *
- *  Contributors:
- *       Metaform Systems, Inc. - initial API and implementation
- *
- */
-
 package org.oaebudt.edc.dcp.core;
 
 import org.eclipse.edc.iam.identitytrust.spi.scope.ScopeExtractorRegistry;

67fcaf5d0a7a6b391b830f67051b2eead907b2cd
mohamedkhalilbeldi <mohamedkhalilbeldi@think-it.io>
2025-04-22 11:38:10 +0100

    fix: fix runtimeOnly deps

--- connector/extensions/catalog-node-resolver/build.gradle.kts
+++ connector/extensions/catalog-node-resolver/build.gradle.kts
@@ -5,8 +5,6 @@ plugins {
 dependencies {
     implementation(libs.edc.spi.identity.did)
     implementation(libs.edc.fc.spi.crawler)
-    runtimeOnly(libs.edc.fc.core)
-    runtimeOnly(libs.edc.fc.api)
 
     testImplementation(libs.edc.junit)
     testImplementation(libs.junit.jupiter)
